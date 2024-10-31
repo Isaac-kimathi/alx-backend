dc49627de9cce7ade1ec8a0a2805b564f836fdc2
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-31 13:03:43 +0300

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,6 +2,8 @@
 """class LIFOCache that inherits from BaseCaching and is a caching system,
 which uses LIFOCache technique"""
 
+from collections import OrderedDict
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 class LIFOCache(BaseCaching):
@@ -9,6 +11,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """innitializes the super class"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """adds to the cache"""
@@ -21,6 +24,7 @@ class LIFOCache(BaseCaching):
                 print("DISCARD: {}". format(last_key))
 
             self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """returns the value in self.cache_data linked to key"""
